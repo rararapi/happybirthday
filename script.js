5530d87f215ff94a61d9ebbd055dfd285d52a7bc
rapirapi <bot0obok9hvu@gmail.com>
2024-02-25 13:43:34 +0900

    editorconfig設定

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function getNameFromURL() {
-    // URLからクエリパラメータを取得
-    const params = new URLSearchParams(window.location.search);
-    const name = params.get('name');
-    return name ? name.toUpperCase() : ''; // 名前を大文字に変換
+  // URLからクエリパラメータを取得
+  const params = new URLSearchParams(window.location.search);
+  const name = params.get("name");
+  return name ? name.toUpperCase() : ""; // 名前を大文字に変換
 }
 
 // ページが読み込まれたときに名前をセット
-document.addEventListener('DOMContentLoaded', function () {
-    const name = getNameFromURL();
-    if (name) {
-        document.getElementById('name').textContent = name;
-    }
-    startConfetti();
+document.addEventListener("DOMContentLoaded", function () {
+  const name = getNameFromURL();
+  if (name) {
+    document.getElementById("name").textContent = name;
+  }
+  startConfetti();
 });
